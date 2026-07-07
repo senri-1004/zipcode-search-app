46de57cac1c9cae100bf205c3028810b85bfc12e
senri-1004 <masa8.masa810@gmail.com>
2026-07-07 11:23:48 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,176 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./page.module.scss";
+
+ type AddressResult = {
+  zipcode: string;
+  address1: string;
+  address2: string;
+  address3: string;
+  kana1: string;
+  kana2: string;
+  kana3: string;
+};
+
+export default function Home() {
+  const [zipcode, setZipcode] = useState("");
+ 
+
+const [result, setResult] = useState<AddressResult | null>(null);
+const [histories, setHistories] = useState<AddressResult[]>([]);
+  const [error, setError] = useState("");
+const [loading, setLoading] = useState(false);
+
+const clearHistory = () => {
+  setHistories([]);
+};
+
+  const handleSearch = async () => {
+    setError("");
+setLoading(true);
+
+const charPattern = /^[0-9-]+$/;
+
+if (!charPattern.test(zipcode)) {
+  setError(
+    "郵便番号は半角数字のみまたは半角数字とハイフンのみで入力してください。"
+  );
+  setLoading(false);
+  return;
+}
+
+const formatPattern = /^\d{7}$|^\d{3}-\d{4}$/;
+
+if (!formatPattern.test(zipcode)) {
+  setError(
+    "郵便番号は半角数字でハイフンありの8桁かハイフンなしの7桁で入力してください。"
+  );
+  setLoading(false);
+  return;
+}
+
+try {
+    const response = await fetch(
+      `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcode.replace("-", "")}`
+    );
+
+  const data = await response.json();
+
+if (!data.results) {
+  setError("郵便番号が存在しません。");
+  setResult(null);
+  return;
+}
+
+const address = data.results[0];
+  setResult(address);
+
+setHistories((prev) => [
+  address,
+  ...prev.filter(
+    (item) => item.zipcode !== address.zipcode
+  ),
+]);
+  } catch {
+    setError("エラーが発生しました。");
+    setResult(null);
+  } finally {
+    setLoading(false);
+  }
+};
+
+  return (
+    <main className={styles.container}>
+      <h1>住所検索</h1>
+
+      <p>
+        郵便番号を入力して住所を検索してください。
+      </p>
+
+      <div className={styles.searchArea}>
+
+        <input
+          type="text"
+          maxLength={8}
+          placeholder="100-0001"
+          value={zipcode}
+          onChange={(e) => setZipcode(e.target.value)}
+  onKeyDown={(e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  }}
+/>
+
+ <button
+  className={styles.searchButton}
+  onClick={handleSearch}
+  disabled={loading || zipcode === ""}
+>
+
+  検索
+</button>
+        {loading && (
+  <p>検索中...</p>
+)}
+      </div>
+      {error && (
+  <p className={styles.error}>
+
+    {error}
+  </p>
+)}
+      {result && (
+  <div className={styles.result}>
+    <h2>検索結果</h2>
+
+    <p>
+      郵便番号: {result.zipcode}
+    </p>
+
+    <p>
+      住所:
+      {result.address1}
+      {result.address2}
+      {result.address3}
+    </p>
+
+    <p>
+      カナ:
+      {result.kana1}
+      {result.kana2}
+      {result.kana3}
+    </p>
+  </div>
+)}
+
+
+{histories.length > 0 && (
+  <div className={styles.history}>
+    <h2>検索履歴</h2>
+
+<button onClick={clearHistory}>
+  履歴を削除
+</button>
+
+    {histories.map((item) => (
+      <div
+        key={item.zipcode}
+        className={styles.historyCard}
+         onClick={() => setResult(item)}
+      >
+        <p>{item.zipcode}</p>
+
+        <p>
+          {item.address1}
+          {item.address2}
+          {item.address3}
+        </p>
+      </div>
+    ))}
+  </div>
+)}
+    </main>
+  );
+}
